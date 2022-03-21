6481dac0335379ac0acf84f5ae5ed878bb9f73cc
Simone Montanari <simone.montanari14@studio.unibo.it>
2022-03-22 00:14:26 +0100

    Cleaning code of Es3.a01a

--- src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
@@ -5,16 +5,31 @@ import scala.util.Random
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01a/sol2/ */
 class LogicsImpl(private val size: Int, private val boat: Int) extends Logics:
 
-  val random = Random ()
-  val randomWithSeed = Random (42)
-
-  var boardColStart = random.nextInt(size - boat)
-  var boardRawStart = random.nextInt(size)
-
-  println("Riga nave --> " + boardRawStart)
-  println("Colonna nave --> " + boardColStart)
+  private val random = Random ()
+  private val randomWithSeed = Random (42)
+  private val vertOrHoriz: Int = random.nextInt(2) // Horizontal = 0 ! Vertical = 1
+  private val boardColStart: Int = random.nextInt(if vertOrHoriz == 0 then size - boat else size)
+  private val boardRawStart: Int = random.nextInt(if vertOrHoriz == 0 then size else size - boat)
+  private val FAILURES: Int = 5
+  private var contHit: Int = 0
+  private var contMiss: Int = 0
 
   def hit(row: Int, col: Int) =
-    if row == boardRawStart && col >= boardColStart && col < (boardColStart + boat) then Result.HIT else Result.MISS
+      if checkCollision(row, col) then
+        this.contHit = this.contHit + 1
+        if this.contHit == boat then Result.WON else Result.HIT
+      else
+        this.contMiss = this.contMiss + 1
+        if this.contMiss == FAILURES then Result.LOST else Result.MISS
 
+  def checkCollision(row: Int, col: Int): Boolean =
+    if this.vertOrHoriz == 0 then
+      row == boardRawStart && col >= boardColStart && col < (boardColStart + boat)
+    else
+      col == boardColStart && row >= boardRawStart && row < (boardRawStart + boat)
 
+/*
+println("Boat row --> " + boardRawStart)
+println("Boat column --> " + boardColStart)
+println(if vertOrHoriz == 0 then "Horizzontal" else "Vertical")
+*/
