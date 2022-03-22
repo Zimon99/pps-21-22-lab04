c4a23d5bf5ea5d0c127fcbb6a133599a40e1cc83
Simone Montanari <simone.montanari14@studio.unibo.it>
2022-03-22 13:15:15 +0100

    Es3 a01b

--- src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
@@ -3,10 +3,38 @@ import scala.jdk.javaapi.OptionConverters
 import u04lab.polyglot.OptionToOptional
 import u04lab.code.Option
 import u04lab.code.Option.*
+import u04lab.code.List
+import u04lab.code.List.*
+import scala.util.Random
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01b/sol2/ */
 class LogicsImpl(private val size: Int, private val mines: Int) extends Logics:
-  def hit(x: Int, y: Int): java.util.Optional[Integer] =
-    OptionToOptional(None()) // Option => Optional converter
 
-  def won = false
+  // Logics of this implement
+  // Example of field with size = 3 and mines = 2
+  //
+  //  0  1  2      When hit is called i 'translate' the coordinates with this formula: row * size + column
+  //  3  4  5      For example:
+  //  6  7  8          row = 1 | column = 2 ---> 1 * 3 + 2 = 5
+
+  private val random = Random ()
+  private var minesPos: List[Int] = Nil()
+  private var contNiceShot: Int = 0
+
+  randomizeMines()
+
+  //println(minesPos)
+
+  def hit(x: Int, y: Int): java.util.Optional[Integer] =
+    if !contains(minesPos, y * size + x) then
+      contNiceShot = contNiceShot + 1
+      OptionToOptional(Some(y * size + x))
+    else
+      OptionToOptional(None()) // Lose
+
+  def won = contNiceShot == (size * size - mines)
+
+  private def randomizeMines(): Unit =
+    while (length(minesPos) != mines)
+      if !contains(minesPos, random.nextInt(size * size)) then
+        minesPos = append(minesPos, Cons(random.nextInt(size * size), Nil()))
